fcffb5c8dca1947ec638119f5cf0c2c6ffa536f5
reproducepark <reproducepark@kaist.ac.kr>
2024-07-27 11:06:51 +0000

    기능 구현

--- src/telbot.py
+++ src/telbot.py
@@ -1,17 +1,48 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import logging, os
+import asyncio, telegram, ping3, time
 from dotenv import load_dotenv
-import os 
 
 load_dotenv()
+botToken = os.environ.get('token')
+chatId = os.environ.get('main_chat_id')
 
-token = os.environ.get('token')
+def GetPingRes(servers):
+    result = []
+    for svr in servers:
+        res = ping3.ping(svr)
+        if res is None or res is False:
+            result.append(-1)
+        else:
+            result.append(res)
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    return result
 
-app = ApplicationBuilder().token(token).build()
+async def send_message(bot):
+    msg = ""
+    servers = ["main", "sub1", "sub2"]
+    xCnt = 0
+    pingRes = GetPingRes(servers)
+    for svr, res in zip(servers, pingRes):
+        if res > 0:
+            sent = f"{svr} ✅ {res:.4f}s\n"
+            logging.info(sent)
+            msg += sent
+        else:
+            xCnt += 1
+            msg += f"{svr} ❌\n"
+    if xCnt > 0:
+        sent = f"{xCnt}/4 not working!"
+        logging.warning(sent)
+        msg += sent
+    await bot.send_message(chatId,msg)
 
-app.add_handler(CommandHandler("hello", hello))
+async def main():
+    bot = telegram.Bot(token = botToken)
+    await send_message(bot)
+    time.sleep(35640)
+    while True:
+        await send_message(bot)
+        time.sleep(43200) # 12시간 대기
 
-app.run_polling()
+if __name__ == "__main__":
+    asyncio.run(main())
